74f54e3d71109e48e97717dc40e462c36a2b5f68
Alice <alicexenawong@gmail.com>
2019-12-18 19:13:29 -0800

    Add search functionality to app.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,17 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <h2>Create New Product</h2>
-    <p>Name:<input type="text" v-model="newProductName"></p> 
-    <p>Description:<input type="text" v-model="newProductDescription"></p>
-    <p>Price:<input type="text" v-model="newProductPrice"></p>
-    <p>Image Url:<input type="text" v-model="newProductImageUrl"></p>
+    <p>Name: <input type="text" v-model="newProductName"></p> 
+    <p>Description: <input type="text" v-model="newProductDescription"></p>
+    <p>Price: <input type="text" v-model="newProductPrice"></p>
+    <p>Image Url: <input type="text" v-model="newProductImageUrl"></p>
     <button v-on:click="addProduct()">Add New Product</button>
-    <div v-for="product in products">
+    <br>
+    <p>Search for Products:<input v-model="searchTerm" list="names"></p>
+    <datalist id="names">
+      <option v-for="product in products">{{ product.name }}</option>
+    </datalist>
+    <div v-for="product in filterBy(products, searchTerm, 'name')">
       <p>{{ product.name }}</p>
       <p>Price: ${{ product.price }}</p>
       <img v-bind:src="product.image_url" width="300px" v-bind:alt="product.name">
@@ -15,6 +20,13 @@
       <button v-on:click="toggleInfo(product)">See more info</button>
       <div v-if="currentProduct === product">
         <p>{{ product.description }}</p>
+        <p>Update Product</p>
+        <p>Name: <input type="text" v-model="product.name"></p>
+        <p>Description: <input type="text" v-model="product.description"></p>
+        <p>Price: <input type="text" v-model="product.price"></p>
+        <p>Image Url: <input type="text" v-model="product.image_url"></p>
+        <button v-on:click="updateProduct(product)">Update Product</button>
+        <button v-on:click="destroyProduct(product)">Destroy Product</button>
       </div>
       <hr>
     </div>
@@ -26,8 +38,10 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Welcome to Vue.js!",
@@ -36,7 +50,8 @@ export default {
       newProductDescription: "",
       newProductPrice: "",
       newProductImageUrl: "",
-      currentProduct: {}
+      currentProduct: {},
+      searchTerm: ""
     };
   },
   created: function() {
@@ -66,7 +81,36 @@ export default {
     },
     toggleInfo: function(theProduct) {
       console.log(theProduct);
-      this.currentProduct = theProduct;
+      if (this.currentProduct === theProduct) {
+        this.currentProduct = null;
+      } else { 
+        this.currentProduct = theProduct;
+      }
+    },
+    updateProduct: function(theProduct) {
+      console.log(theProduct);
+
+      var params = {
+        name: theProduct.name,
+        description: theProduct.description,
+        price: theProduct.price,
+        image_url: theProduct.image_url
+      };
+      axios.patch(`/api/products/${theProduct.id}`, params).then(response => {
+        console.log(response.data);
+        theProduct.name = response.data.name;
+        theProduct.description = response.data.description;
+        theProduct.price = response.data.price;
+        theProduct.image_url = response.data.image_url;
+      });
+    },
+    destroyProduct: function(theProduct) {
+      console.log(theProduct);
+      axios.delete(`api/products/${theProduct.id}`).then(reponse => {
+        var index = this.products.indexOf(theProduct);
+        console.log(index);
+        this.products.splice(index, 1);
+      });
     }
   }
 };
